81a634aec27160435f0221aca7bc346486240b4a
Dplant <dean.plant@greenpeace.org>
2016-01-21 11:26:36 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ import lxml.html
 
 # # Read in a page
 html = scraperwiki.scrape("http://uk.soccerway.com/teams/netherlands/fortuna-sittard/")
-print html
+#print html
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
 root.cssselect("td")
