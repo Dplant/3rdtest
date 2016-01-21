c92e56d2bd03b2431055cb84756b5e5653e4cca7
Dplant <dean.plant@greenpeace.org>
2016-01-21 12:21:18 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,10 +10,6 @@ html = scraperwiki.scrape("http://uk.soccerway.com/teams/netherlands/fortuna-sit
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
 tds = root.cssselect("td")
-for td in tds:
-  record = {"cell" : td.text}
-  print record
-  scraperwiki.sqlite.save(['cell'], record)
 indexno = 0
 for td in tds:
   indexno = indexno + 1
